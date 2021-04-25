4fb7b90b85d6bd0158cc45a8fbaa151ec01773ec
kmlee <kmlee9243@gmail.com>
2021-04-25 18:04:46 +0900

    Repository 생성 및 JPA테스트

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity // == table
+// @Table (해당 클래스명이 스키마의 테이블명과 같다면 table 어노테이션 상관없음)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}

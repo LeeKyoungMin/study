9843a389b2d48892b829e9c5d11ec256f3219bcd
kmlee <kmlee9243@naver.com>
2021-04-26 16:58:21 +0900

    crud 테스트

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -2,6 +2,7 @@ package com.example.study.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity // == table
 // @Table (해당 클래스명이 스키마의 테이블명과 같다면 table 어노테이션 상관없음)
 public class User {
@@ -18,6 +20,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String password;
+    private String status;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
     private String account;
     private String email;
     private String phoneNumber;
--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -2,26 +2,78 @@ package com.example.study.repository;
 
 import com.example.study.StudyApplication;
 import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
+    //Dependency Injection (DI)
     @Autowired
     private UserRepository userRepository;
 
+    @Test
     public void create(){
+        // String sql = insert into user (%s, %s, %d) value (account, email, age);
+        User user = new User();
+        user.setAccount("testUser02");
+        user.setEmail("testUser02@gmail.com");
+        user.setPassword("1235");
+        user.setStatus("Right");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : " + newUser);
+    }
+
+    @Test
+    public void read(@RequestParam Long id){
+        Optional<User> user = userRepository.findById(id);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
 
     }
 
-    public void read(){
-
-    }
-
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
+    @Test
     public void delete(){
+        Optional<User> user = userRepository.findById(2L);
+
+        Assert.isTrue(user.isPresent());
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+
+        if(deleteUser.isPresent()){
+            System.out.println("데이터 존재 : " + deleteUser.get());
+        }else{
+            System.out.println("데이터 삭제 데이터 없음");
+        }
 
     }
 }

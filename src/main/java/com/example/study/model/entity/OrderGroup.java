5bcd7a8922eb567641376f3e2afd26157f069e18
kmlee <kmlee9243@naver.com>
2021-05-11 18:26:39 +0900

    enum 및 추상화 정리

--- src/main/java/com/example/study/model/entity/OrderGroup.java
+++ src/main/java/com/example/study/model/entity/OrderGroup.java
@@ -1,5 +1,6 @@
 package com.example.study.model.entity;
 
+import com.example.study.model.enumclass.OrderType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,7 +30,8 @@ public class OrderGroup {
 
     private String status;
 
-    private String orderType;   // 주문의 형태 - 일괄 / 개별
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;   // 주문의 형태 - 일괄 / 개별
 
     private String revAddress;
 
--- src/main/java/com/example/study/service/ItemApiLogicService.java
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -11,9 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
-public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiLogicService extends BaseService<ItemApiRequest, ItemApiResponse, Item> {
 
     @Autowired
     private PartnerRepository partnerRepository;
@@ -24,9 +25,8 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
     @Override
     public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
 
-        ItemApiRequest body = request.getData();
-
-        Item item = Item.builder()
+        return Optional.ofNullable(request.getData())
+                .map(body->{ Item item = Item.builder()
                 .status(body.getStatus())
                 .name(body.getName())
                 .title(body.getTitle())
@@ -37,15 +37,17 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
                 .partner(partnerRepository.getOne(body.getPartnerId()))
                 .build();
 
-        Item newItem = itemRepository.save(item);
-
-        return response(newItem);
+        return item;
+    })
+        .map(newItem -> baseRepository.save(newItem))
+        .map(newItem -> response(newItem))
+        .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     @Override
     public Header<ItemApiResponse> read(Long id) {
 
-        return itemRepository.findById(id)
+        return baseRepository.findById(id)
                 .map(item -> response(item))
                 .orElseGet(() -> Header.ERROR("데이터 없음"));
 
@@ -56,7 +58,7 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
 
         ItemApiRequest body = request.getData();
 
-        return itemRepository.findById(body.getId())
+        return baseRepository.findById(body.getId())
                 .map(entityItem->{
                     entityItem.setStatus(body.getStatus())
                                 .setName(body.getName())
@@ -70,7 +72,7 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
                     return entityItem;
 
                 })
-                .map(newEntityItem ->itemRepository.save(newEntityItem))
+                .map(newEntityItem ->baseRepository.save(newEntityItem))
                 .map(item -> response(item))
                 .orElseGet(()->Header.ERROR("데이터 없음"));
 
@@ -79,8 +81,8 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
     @Override
     public Header delete(Long id) {
 
-        return itemRepository.findById(id)
-                .map(item -> {itemRepository.delete(item);
+        return baseRepository.findById(id)
+                .map(item -> {baseRepository.delete(item);
                     return Header.OK();
                 })
                 .orElseGet(()->Header.ERROR("데이터 없음"));
